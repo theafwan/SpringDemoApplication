09674bc5a5912643c0e44a7c0dd4b5d7490c4bbd
Mohammed Afwan <afwan.md2@gmail.com>
2022-11-08 22:33:17 +0100

    Exposing a service Layer through Rest

--- src/main/java/com/example/demo/business/ReservationService.java
+++ src/main/java/com/example/demo/business/ReservationService.java
@@ -78,5 +78,25 @@ public class ReservationService {
         });
         return guestList;
     }
+
+    public void addHotelGuest(Guest guest){
+        if(null == guest){
+            throw new RuntimeException("Guest Cannot be null");
+        }
+        this.guestRepository.save(guest);
+    }
+
+    public List<Room> getRooms(){
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        List<Room> roomList = new ArrayList<>();
+        rooms.forEach(room -> {roomList.add(room);});
+        roomList.sort(new Comparator<Room>() {
+            @Override
+            public int compare(Room o1, Room o2) {
+                return o1.getRoomNumber().compareTo(o2.getRoomNumber());
+            }
+        });
+        return roomList;
+    }
 }
 
--- src/main/java/com/example/demo/webservice/WebserviceController.java
+++ src/main/java/com/example/demo/webservice/WebserviceController.java
@@ -2,11 +2,11 @@ package com.example.demo.webservice;
 
 import com.example.demo.business.ReservationService;
 import com.example.demo.business.RoomReservation;
+import com.example.demo.data.Guest;
+import com.example.demo.data.Room;
 import com.example.demo.util.DateUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -26,5 +26,21 @@ public class WebserviceController {
     public List<RoomReservation> getReservations(@RequestParam(value = "date", required = false) String dateString){
         Date date = this.dateUtils.createDateFromDateString(dateString);
         return this.reservationService.getRoomReservationsForDate(date);
+    }
+
+    @GetMapping(value = "/guests")
+    public  List<Guest> getGuests(){
+        return this.reservationService.getHotelGuests();
+    }
+
+    @PostMapping(value = "/guests")
+    @ResponseStatus(HttpStatus.CREATED)
+    public  void addGuest(@RequestBody Guest guest){
+        this.reservationService.addHotelGuest(guest);
+    }
+
+    @GetMapping("/rooms")
+    public List<Room> getRooms(){
+        return this.reservationService.getRooms();
     }
 }
